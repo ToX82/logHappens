e8242df7744ff573fa1dcae4252e17b55f6a2ddc
tox <tox@localhost.localdomain>
2017-04-26 19:24:46 +0200

    Better template files

--- logics_templates/apache24.php
+++ logics_templates/apache24.php
@@ -14,26 +14,32 @@ $content = file($menu['file']);
 
 $log = [];
 foreach ($content as $line) {
-    // Grab the log's time and group logs by time
-    $time = substr($line, 1, 19);
-    $time = date("l d-m-Y - H:i:s", strtotime($time));
+    if (substr($line, 0, 1) != "[") {
+        $log[$time][] = str_replace("\n", "", $line);
+    } else {
+        // Grab the log's time and group logs by time
+        $time = substr($line, 1, 19);
+        $time = date("l d-m-Y - H:i:s", strtotime($time));
 
-    if ($time != "") {
-        // Remove date-time and other useless informations from the log details
-        $line = substr($line, 34);
-        $line = preg_replace("[\[:error.*\]]", "", $line);
-        $line = preg_replace("[\[pid .*\]]", "", $line);
-        $line = str_replace("PHP", "", $line);
+        if ($time != '') {
+            // Remove date-time and other useless informations from the log details
+            $line = substr($line, 34);
+            $line = preg_replace('[\[:error.*\]]', '', $line);
+            $line = preg_replace('[\[pid .*\]]', '', $line);
+            $line = str_replace('PHP', '', $line);
+            $line = trim($line);
 
-        // Highlight the type of errors, using a badge
-        $line = preg_replace("/^Notice: /", "<span class='lh-badge' style='background-color: #318418;'>Notice:</span> ", $line);
-        $line = preg_replace("/^Warning: /", "<span class='lh-badge' style='background-color: #a79716;'>Warning:</span> ", $line);
-        $line = preg_replace("/^Fatal error: /", "<span class='lh-badge' style='background-color: #a71616;'>Fatal error:</span> ", $line);
-        $line = preg_replace("/^Parse error: /", "<span class='lh-badge' style='background-color: #a71616;'>Parse error:</span> ", $line);
-        $line = preg_replace("/^Error: /", "<span class='lh-badge' style='background-color: #a71616;'>Error:</span> ", $line);
+            // Highlight the type of errors, using a badge
+            $line = preg_replace("/^Debug_log: /", "<span class='lh-badge' style='background-color: #1e88e5;'>Debug_log:</span> ", $line);
+            $line = preg_replace("/^Notice: /", "<span class='lh-badge' style='background-color: #318418;'>Notice:</span> ", $line);
+            $line = preg_replace("/^Warning: /", "<span class='lh-badge' style='background-color: #a79716;'>Warning:</span> ", $line);
+            $line = preg_replace("/^Fatal error: /", "<span class='lh-badge' style='background-color: #a71616;'>Fatal error:</span> ", $line);
+            $line = preg_replace("/^Parse error: /", "<span class='lh-badge' style='background-color: #a71616;'>Parse error:</span> ", $line);
+            $line = preg_replace("/^Error: /", "<span class='lh-badge' style='background-color: #a71616;'>Error:</span> ", $line);
 
-        // Save the log entry
-        $log[$time][] = trim($line);
+            // Save the log entry
+            $log[$time][] = $line;
+        }
     }
 }
 
--- logics_templates/cakephp.php
+++ logics_templates/cakephp.php
@@ -17,18 +17,22 @@ foreach ($content as $line) {
     // Don't print the datetime for every record
     if (substr($line, 0, 3) == "201") {
         $time = substr($line, 0, 20);
-        $time = date("l d-m-Y - H:i:s", strtotime($time));
-
         $line = str_replace($time, "", $line);
     }
+    $time = date("l d-m-Y - H:i:s", strtotime($time));
+    $line = trim($line);
 
     // Highlight the type of errors, using a badge
+    $line = preg_replace("/^Debug_log: /", "<span class='lh-badge' style='background-color: #1e88e5;'>Debug_log:</span> ", $line);
     $line = preg_replace("/^Notice: /", "<span class='lh-badge' style='background-color: #318418;'>Notice:</span> ", $line);
     $line = preg_replace("/^Warning: /", "<span class='lh-badge' style='background-color: #a79716;'>Warning:</span> ", $line);
     $line = preg_replace("/^Fatal error: /", "<span class='lh-badge' style='background-color: #a71616;'>Fatal error:</span> ", $line);
     $line = preg_replace("/^Parse error: /", "<span class='lh-badge' style='background-color: #a71616;'>Parse error:</span> ", $line);
     $line = preg_replace("/^Error: /", "<span class='lh-badge' style='background-color: #a71616;'>Error:</span> ", $line);
 
-    $log[$time][] = trim($line);
+    // Save the log entry
+    $log[$time][] = $line;
 }
+
+// Reverse the logs, so that we can see last errors first
 $logs = array_reverse($log);
