68d60619f8d130a753b8a0acaafb4380fdd3076e
Emanuele "ToX" Toscano <toss82@gmail.com>
2020-02-17 23:57:28 +0100

    Fix codeigniter log parser

--- parsers/codeigniter.php
+++ parsers/codeigniter.php
@@ -13,14 +13,14 @@ foreach ($content as $line) {
     } else {
         continue;
     }
-    $lineStart = strpos($line, " --> Severity: ") + 15;
-    $line = substr($line, $lineStart, 999999);
+
     $line = trim($line);
 
     // Highlight the type of errors, using a badge
-    $line = preg_replace("/^Notice --> /", "<span class='lh-badge' style='background-color: #318418;'>Notice:</span> ", $line);
-    $line = preg_replace("/^Warning --> /", "<span class='lh-badge' style='background-color: #a79716;'>Warning:</span> ", $line);
-    $line = preg_replace("/^Error --> /", "<span class='lh-badge' style='background-color: #a71616;'>Error:</span> ", $line);
+    $line = preg_replace("/^NOTICE - /", "<span class='lh-badge' style='background-color: #318418;'>Notice:</span> ", $line);
+    $line = preg_replace("/^WARNING - /", "<span class='lh-badge' style='background-color: #a79716;'>Warning:</span> ", $line);
+    $line = preg_replace("/^INFO - /", "<span class='lh-badge' style='background-color: #a79716;'>Info:</span> ", $line);
+    $line = preg_replace("/^ERROR - /", "<span class='lh-badge' style='background-color: #a71616;'>Error:</span> ", $line);
 
     // Save the log entry
     $logs[$time][] = $line;
